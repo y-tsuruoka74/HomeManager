015dc1cc5a26537839af580b7cee7c4936a2eb9a
Yuki Tsuruoka <y-tsuruoka@sakura.ad.jp>
2026-03-25 17:37:37 +0900

    chore: Neovim の相対行番号設定を無効化
    - dotfiles/nvim/lua/config/options.lua:
      - `vim.opt.relativenumber = false` を追加し、相対行番号表示を無効化

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.relativenumber = false
